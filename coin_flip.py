9b008a7c9df4495b75f0b0d936b10eab0031b7b1
Anna <55453776+annaswedrak@users.noreply.github.com>
2020-04-17 23:58:08 +0200

    the challenge is working :3

--- coin_flip.py
+++ coin_flip.py
@@ -24,38 +24,36 @@ def coin_flip():
 #again after each cycle.
 
 def coin_flip2():
-    correct_count = 0
-    total_count = 0
+    correct_count1 = 0
+    total_count1 = 0
     while True:
         print("Guess the outcome of a random coin flip (heads/tails)")
-        guess = input()
+        guess1 = input()
         possible_choices = ["heads", "tails"]
         flip = random.choice(possible_choices)
         print("The result of a coin flip is " + flip)
-        if guess == flip:
+        if guess1 == flip:
             print("You guessed correctly!")
-            correct_count += 1
-            total_count += 1
+            correct_count1 += 1
+            total_count1 += 1
         else:
             print("You didn't guessed correctly!")
-            total_count += 1
-        print("Total count of your guesses is " + str(total_count))
-        print("The number of correct guesses is " + str(correct_count))
+            total_count1 += 1
+        print("Total count of your guesses is " + str(total_count1))
+        print("The number of correct guesses is " + str(correct_count1))
         print("Do you want to play again? Type y for yes and n for no")
         again = input()
         if again == "n":
             break
-        else:
-            continue
-    return correct_count, total_count
+    return correct_count1, total_count1
 
-#coin_flip2()
+
 #Let’s see if we can expand upon this challenge - what if
 #instead of 2 options, there were 6?
 #User Story: As a user I want to be able to guess the outcome of a
 #6-sided dice roll (1-6), with the same feature set as the coin flip
 #(see above).
-
+#s
 #You can add this directly to the existing program you’ve already written!
 #As an additional challenge see if you can build the program such that the
 #the user can choose between the two
@@ -81,14 +79,36 @@ def dice_roll():
         again = input()
         if again == "n":
             break
-        else:
-            continue
     return correct_count, total_count
 
 
+
+
 def playing_game():
+    coin_flip_correct = 0
+    coin_flip_total = 0
+    dice_roll_correct = 0
+    dice_roll_total = 0
     while True:
         print("Which game do you want to play?")
         print("Press 1 for coin flip, 2 for 6 - sided dice roll and 3 to quit")
         game = int(input())
-        
+        if game == 1:
+            a, b = coin_flip2()
+            coin_flip_correct += a
+            coin_flip_total += b
+            print("You scored " + str(coin_flip_correct) + " correct guesses in the coin flip game overall.")
+            print("You had " + str(coin_flip_total) + " total number of guesses in the coin flip game.")
+        elif game == 2:
+            e, f = dice_roll()
+            dice_roll_correct += e
+            dice_roll_total += f
+            print("You scored " + str(dice_roll_correct) + " corrects guesses in the dice roll game.")
+            print("You had " + str(dice_roll_total) + " total number of guesses in the dice roll game")
+        else:
+            break
+    print("Thank you for your game.")
+    print("You scored " + str(coin_flip_correct + dice_roll_correct) + " corrects guesses in both games.")
+    print("You tried " + str(coin_flip_total + dice_roll_total) + " times.")
+
+playing_game()
